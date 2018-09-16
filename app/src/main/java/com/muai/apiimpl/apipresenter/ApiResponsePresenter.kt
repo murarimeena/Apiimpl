5226d407b158a8815e2b35170bb6343cdc60170e
Murari Meena <35348001+murarimeena@users.noreply.github.com>
2018-09-16 10:17:52 +0530

    Update ApiResponsePresenter.kt
    File modified from github

--- app/src/main/java/com/muai/apiimpl/apipresenter/ApiResponsePresenter.kt
+++ app/src/main/java/com/muai/apiimpl/apipresenter/ApiResponsePresenter.kt
@@ -17,11 +17,14 @@ interface ApiResponsePresenter {
         call.enqueue(object : Callback() {
             fun onResponse(response: Response, retrofit: Retrofit) {
                 iResponseInterface.onResponseSuccess(response, reqType)
+                throwable.printStackTrace()
+                    iResponseInterface.onResponseFailure("Response Failed")
             }
 
             fun onFailure(throwable: Throwable) {
                 if (throwable is ConnectException) {
                     iResponseInterface.onResponseFailure("No Internet Connection")
+                    iResponseInterface.onResponseSuccess(response, reqType)
                 } else {
                     throwable.printStackTrace()
                     iResponseInterface.onResponseFailure("Response Failed")
